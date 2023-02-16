e05f2583cff5ce74ce9fdf689cf251ac94df5f83
Alvaro <alvarogharo@gmail.com>
2023-02-16 12:54:53 +0100

    Added compiled lib url

--- Package.swift
+++ Package.swift
@@ -12,8 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/3.1.0/WalletCore.xcframework.zip",
-            checksum: "2487af30a8f6f4775a41f29456f792f15269f4f4daae2da7e4b9ef8747613e3a"
+            url: "https://github.com/GeoDB-Limited/wallet-core-odin/releases/download/3.0.5/WalletCoreCommon.xcframework.zip",
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
